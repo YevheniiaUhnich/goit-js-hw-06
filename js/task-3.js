a9fcd9a6a8c04766a750710e7f73f02d99905549
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2023-11-23 20:49:48 +0200

    hw6

---
+++ js/task-3.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
